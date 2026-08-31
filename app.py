307ecdd4dcfe79cf6a41799e1e4681c5614f21f4
cdube001 <cdube001@ucr.edu>
2026-08-31 02:16:38 -0700

    Checking Price dataframe with test

--- app.py
+++ app.py
@@ -939,6 +939,9 @@ def price_histogram(market_data):
     
     st.write(free_data)
     st.write(paid_data)
+
+    paid_data["sort_price"] = (paid_data["Price"].str.replace("$","", regex=False).astype(float))
+    st.write(paid_data)
     # figure = px.histogram(price_data, x="initial_price", nbins=20,title="Price Distribution Among Similar Games", labels={"current_price": " Price($)"})
     # figure.update_layout(xaxis_title="Price ($)", yaxis_title="Number of Games")
     # st.plotly_chart(figure, use_container_width=True)
